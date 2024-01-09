47c23f827a8ddc781f768220af5caa50a28f78a1
Godson David <davidgsongs@gmail.com>
2024-01-09 14:41:31 +0100

    Done

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'LittleLemon',
-        'USER': 'admindjango',
-        'PASSWORD': 'employee@123!',
+        'NAME': 'little',
+        'USER': 'Davysongs',
+        'PASSWORD': '#$(tVA0nr)',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
