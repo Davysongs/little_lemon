fe0d8dd68d9e562f0fdabcac8cee28bfe18da8a8
Davysongs <Davidgsongs@gmail.com>
2024-01-12 09:42:15 +0100

    Page connections

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -26,6 +26,12 @@ router.register(r'tables', views.BookingViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name= "home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    path('reservations/', views.reservations, name="reservations"),
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
+    path('bookings', views.bookings, name='bookings'), 
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
     path('users', UserViewSet.as_view({'post': 'create'}), name="register"),
