4f5b567b74172b1fdde81634c267ef9ab79d0e8e
Davysongs <Davidgsongs@gmail.com>
2024-03-31 12:51:46 +0100

    Deploy3

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,14 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Install Python
+apt-get update
+apt-get install -y python3 python3-pip
+
+# Install dependencies
+pip3 install -r requirements.txt
+python3 manage.py migrate
+
+
+# Build Django app or collect static files
+
+python3 manage.py collectstatic --noinput
