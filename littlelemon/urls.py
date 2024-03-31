4e77ea55becb8b6efeb19b8d1b5a92ff80b16546
Davysongs <Davidgsongs@gmail.com>
2024-03-31 12:37:48 +0100

    Deploy

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,24 +1,11 @@
-"""
-URL configuration for littlelemon project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from restaurant import views
 from djoser.views import UserViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
@@ -38,4 +25,4 @@ urlpatterns = [
     path('users/users/me', UserViewSet.as_view({'get': 'list'}), name='user-view'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
